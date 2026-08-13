09aecbd9c485d65ea605d3c2ad27045f66f7fc68
joaovictorferreira1703-blip <joaovictorferreira1703@gmail.com>
2026-08-13 11:26:10 -0300

    feat: adiciona as credenciais para a conexão do banco de dados

---
+++ js/supabase_config.js
@@ -0,0 +1,4 @@
+const supabase_url = 'https://rofuwkpdvatadnivgmau.supabase.co' // url do banco de dados
+const supabase_key = 'sb_publishable_bjusFyhjP6bNohL4hs5fhg_csZFmlwF' // chave pública
+
+const supabase = supabase.createClient(supabase_url, supabase_key);
